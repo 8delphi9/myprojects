37f6ddc45b377ea5603a60db84df1f7611582843
Yong Su Jeong <knarrblue@gmail.com>
2022-07-06 13:17:25 +0900

    Feat - 회원가입 api 완료

--- user/views.py
+++ user/views.py
@@ -37,3 +37,25 @@ class LoginApi(APIView):
 
         response = Response(status=status.HTTP_200_OK)
         return jwt_login(response, user)
+
+
+class UserCreateApi(APIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, **kwargs):
+        """
+        author: 정용수
+        회원가입: 이메일, 패스워드, 닉네임
+        :param request: QueryDict
+        :return: JSON
+        """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({
+                "message": "회원 가입이 완료되었습니다."
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "message": "이메일 또는 패스워드 또는 닉네임 형식을 확인해주세요."
+        }, status=status.HTTP_400_BAD_REQUEST)
