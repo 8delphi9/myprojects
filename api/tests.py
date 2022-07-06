68c742afcd146392e3038d0748660cba2e36e1a0
jay <jaewhan232@naver.com>
2022-07-06 09:45:09 +0900

    Test - 기록 조회, 등록 테스트 구현 기록 리스트 조회 : response status_code, 반환 자료의 타입 검사 기록 등록 : response status_code 검하

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from rest_framework.test import (APITestCase, APIClient) 
+from rest_framework import status
+from datetime import date
+
+import json
 
 # Create your tests here.
+
+class RecordListViewTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_record_list(self):
+        result = self.client.get('http://127.0.0.1:8000/api/ledgers', content_type='application/json')
+        print(result.content)
+        result_content = json.loads(result.content.decode('utf-8'))
+        print(result)
+        
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(type(result_content), type([]))
+
+    def test_post_record(self):
+        data = {
+            'date': date.today(),
+            'amount': 20000,
+            'in_ex': 'expense',
+            'method': 'cash',
+        }
+        
+        result = self.client.post('http://127.0.0.1:8000/api/ledgers/', data, content_type='application/json')
+        
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        
+        
