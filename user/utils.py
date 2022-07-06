00b401e2e9dd079283d0c10573cb6f2a9db8b294
SMin1620 <ibb09@naver.com>
2022-07-06 09:19:56 +0900

    feat :  user utils.py 생성 로그인 시 마지막 로그인 기록하도록 유틸을 설정

---
+++ user/utils.py
@@ -0,0 +1,12 @@
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+def get_user_login(user: User):
+    user.last_login = timezone()
+    user.save()
+
+    return user
