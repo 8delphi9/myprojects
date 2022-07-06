176d6f216a36c7640314c107381f6ed525dec39f
jay <jaewhan232@naver.com>
2022-07-06 10:31:21 +0900

    Test - 경기 리스트 조회 테스트 성공 케이스 테스트 구현

--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,5 @@
-from rest_framework.test import (APITestCase, APIClient) 
+from rest_framework.test import (APITestCase,
+                                 APIClient) 
 from rest_framework import status
 from datetime import date
 
@@ -11,24 +12,9 @@ class RecordListViewTest(APITestCase):
         self.client = APIClient()
 
     def test_get_record_list(self):
-        result = self.client.get('http://127.0.0.1:8000/api/ledgers', content_type='application/json')
-        print(result.content)
-        result_content = json.loads(result.content.decode('utf-8'))
-        print(result)
-        
+        result = self.client.get('http://127.0.0.1:8000/api/ledgers/')
+                
         self.assertEqual(result.status_code, status.HTTP_200_OK)
-        self.assertEqual(type(result_content), type([]))
 
-    def test_post_record(self):
-        data = {
-            'date': date.today(),
-            'amount': 20000,
-            'in_ex': 'expense',
-            'method': 'cash',
-        }
-        
-        result = self.client.post('http://127.0.0.1:8000/api/ledgers/', data, content_type='application/json')
-        
-        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
         
         
