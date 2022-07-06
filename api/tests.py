72bb94bfc77de271fa5e1a92b634a8b1a0766e01
Jaewhan Jeon <67890930+iamjaewhan@users.noreply.github.com>
2022-07-06 10:44:38 +0900

    Revert "Feature/record lookup"

--- api/tests.py
+++ api/tests.py
@@ -1,20 +1,3 @@
-from rest_framework.test import (APITestCase,
-                                 APIClient) 
-from rest_framework import status
-from datetime import date
-
-import json
+from django.test import TestCase
 
 # Create your tests here.
-
-class RecordListViewTest(APITestCase):
-    def setUp(self):
-        self.client = APIClient()
-
-    def test_get_record_list(self):
-        result = self.client.get('http://127.0.0.1:8000/api/ledgers/')
-                
-        self.assertEqual(result.status_code, status.HTTP_200_OK)
-
-        
-        
