4aea9b7bb03610383edd6958084aba55035097e7
jay <jaewhan232@naver.com>
2022-07-07 20:10:39 +0900

    Refactor - 가계부 기록 params 설명 수정 date -> YYYY-MM-DD

--- ledger/ledger_api_params.py
+++ ledger/ledger_api_params.py
@@ -3,7 +3,7 @@ from drf_yasg import openapi
 record_post_params = openapi.Schema(
     type=openapi.TYPE_OBJECT,
     properties={
-        'date': openapi.Schema(type=openapi.FORMAT_DATE, description='date'),
+        'date': openapi.Schema(type=openapi.FORMAT_DATE, description='YYYY-MM-DD'),
         'amount': openapi.Schema(type=openapi.TYPE_INTEGER, description='amount'),
         'in_ex': openapi.Schema(type=openapi.TYPE_STRING, description='income/expense'),
         'method': openapi.Schema(type=openapi.TYPE_STRING, description='cash/card/transfer'),
