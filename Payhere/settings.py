3e394d955e4e1a34ac231a8b055d07e5de1f8d73
SMin1620 <ibb09@naver.com>
2022-07-07 21:56:43 +0900

    Feat : Docker관련 db전용 env와 도커 세팅

--- Payhere/settings.py
+++ Payhere/settings.py
@@ -149,8 +149,8 @@ DATABASES = {
         'NAME': env('MYSQL_DATABASE'),
         'USER': env('MYSQL_USER'),
         'PASSWORD': env('MYSQL_ROOT_PASSWORD'),
-        'HOST': '127.0.0.1',
-        # 'HOST': 'db',
+        # 'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '3306',
         'OPTIONS': {
             # 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # 추가, 만약에 이 부분 때문에 오류가 난다면, 이 라인을 지우고 다시 시도해주세요.
