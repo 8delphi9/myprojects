b9c5d33d91f42063bf3f3fb954fbbafef1b1c545
jay <jaewhan232@naver.com>
2022-07-07 11:20:36 +0900

    Refactor - views.py 이동 api  앱에서 ledgers 앱으로 이동

---
+++ ledger/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import *
+
+
+class RecordSummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Record
+        fields = ['id', 'date', 'amount', 'in_ex']
+
+
+class LedgerDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Record
+        fields = ('id', 'date', 'in_ex', 'amount', 'method', 'memo')
