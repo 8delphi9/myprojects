e73d3d3574f63aafd8879db31d7a94cfc81e4537
SMin1620 <ibb09@naver.com>
2022-07-08 11:24:37 +0900

    Feat : User Test Case 유저 테스트 구현

--- user/tests.py
+++ user/tests.py
@@ -6,32 +6,41 @@ from user.models import User
 
 
 # Create your tests here.
-class Test(APITestCase):
+class UserTest(APITestCase):
     client = APIClient()
+    headers = {}
 
     def setUp(self):
-        super_user = User.objects.create_superuser('admin@google.com', 'admin', 'adminpass')
-        self.user = super_user
+        # superuser
+        super_user = User.objects.create_superuser(
+            email='admin@google.com',
+            nickname='admin_test',
+            password='adminpass'
+        )
+        super_user.save()
+        self.admin = super_user
 
-        user = User.objects.create_user('test5@gmail.com','test5','Testtest1@')
+        user = User.objects.create_user('test5@gmail.com', 'test5', 'Testtest1@')
 
         user.save()
         self.user = user
         self.assertIsInstance(user, User)
 
-    def test_signup(self):
-        context = {
-            'email': 'test2@gmail.com',
-            'password1': 'Testtest1@',
-            'password2': 'Testtest1@',
-            'nickname': 'test2'
+        # herders
+        data = {
+            'email': 'admin@google.com',
+            'password': 'adminpass'
         }
-        response = self.client.post(
-            f'/api/user/signup/',
-            json.dumps(context),
-            content_type='application/json'
-        )
+        response = self.client.post('/api/user/login/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
+
+        self.token_access = response.data['token']['access']
+        self.assertNotEqual(self.token_access, '')
+
+        self.headers = {
+            'HTTP_Authorization': 'Bearer ' + self.token_access,
+        }
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token_access}')
 
     def test_user_update(self):
         login_context = {
@@ -81,3 +90,71 @@ class Test(APITestCase):
         )
 
         self.assertEqual(response.status_code, 200)
+
+    ###### Test Login ######
+    def test_success_admin_login(self):
+        user = {
+            'email': 'admin@google.com',
+            'password': 'adminpass'
+        }
+
+        response = self.client.post('/api/user/login/', json.dumps(user), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_fail_admin_login(self):
+        user = {
+            'email': 'admin@google.com',
+            'password': 'fallpassword'
+        }
+
+        response = self.client.post('/api/user/login/', json.dumps(user), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_success_user_login(self):
+        user = {
+            'email': 'test5@gmail.com',
+            'password': 'Testtest1@'
+        }
+
+        response = self.client.post('/api/user/login/', json.dumps(user), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_fail_user_login(self):
+        user = {
+            'email': 'test5@gmail.com',
+            'password': 'Test'
+        }
+
+        response = self.client.post('/api/user/login/', json.dumps(user), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_token_login(self):
+        user = {
+            'email': 'admin@google.com',
+            'password': 'adminpass'
+        }
+
+        response = self.client.post('/api/user/login/', json.dumps(user), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        self.token_access = response.data['token']['access']
+        self.token_refresh = response.data['token']['refresh']
+        self.assertNotEqual(self.token_access, '')
+        self.assertNotEqual(self.token_refresh, '')
+
+    ##### 어드민전용 #####
+    def test_read_success_admin(self):
+        response = self.client.get('/api/admin/user/', **self.headers, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_read_fail_admin(self):
+        self.client.credentials(HTTP_AUTHORIZATION='')
+
+        response = self.client.get('/api/admin/user/', **self.headers, content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+
+
+
+
+
+
