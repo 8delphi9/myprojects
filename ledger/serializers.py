07829db0ebd32e1e4a617301862539d298279abd
jay <jaewhan232@naver.com>
2022-07-08 16:38:29 +0900

    Docs - 코드 작성자 추가

--- ledger/serializers.py
+++ ledger/serializers.py
@@ -3,17 +3,29 @@ from .models import *
 
 
 class RecordSummarySerializer(serializers.ModelSerializer):
+    """
+    author : 전재완
+    explanation : 가계부의 요약 정보 serializer
+    """
     class Meta:
         model = Record
         fields = ['id', 'date', 'amount', 'in_ex']
 
 
 class LedgerDetailSerializer(serializers.ModelSerializer):
+    """
+    author :
+    explanation :
+    """
     class Meta:
         model = Record
         fields = ('id', 'date', 'in_ex', 'amount', 'method', 'memo')
         
 class RecordModifySerializer(serializers.ModelSerializer):
+    """
+    author : 전재완
+    explanation : 가계부 수정 serializer
+    """
     class Meta:
         model = Record
         fields = ('amount', 'memo')
