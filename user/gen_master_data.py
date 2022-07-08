a4344370eecaaec1a46189eed4ef36603729f92b
SMin1620 <ibb09@naver.com>
2022-07-08 14:35:34 +0900

    Docs : 인증, 퍼미션, 더미데이터에 주석 추가

--- user/gen_master_data.py
+++ user/gen_master_data.py
@@ -7,6 +7,14 @@ User = get_user_model()
 
 # 유저 더미데이터
 def gen_master(apps, schema_editor):
+    """
+    author : 이승민
+    explanation :
+         - 유저 더미데이터
+         - migration 파일에 적용시켜 migrate하면 유저를 자동으로 생성하게 해준다.
+         - 슈퍼유저, 일반 유저 2~4
+    """
+
     # 슈퍼 유저 생성
     User.objects.create_superuser(
         email='admin@email.com',
